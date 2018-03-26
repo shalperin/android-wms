40ae91a1c62b03d6d859f8c62d977e133d155d06
Sam Halperin <sqh@me.com>
2018-03-25 21:59:19 -0400

    remove extraneous code from Main

--- WMSDemo/app/src/main/java/us/blauha/wmsdemo/MainActivity.java
+++ WMSDemo/app/src/main/java/us/blauha/wmsdemo/MainActivity.java
@@ -19,34 +19,18 @@ public class MainActivity extends android.support.v4.app.FragmentActivity {
      *  Create a debugging Maps API Key and add it to the manifest.
      *
      */
-    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpMapIfNeeded();
-    }
-
-    private void setUpMapIfNeeded() {
-        if (mMap == null) {
-            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            mMap = googleMap;
-                            setUpMap();
-                        }
-                    });
-        }
-    }
-
-    private void setUpMap() {
-        TileProvider wmsTileProvider = TileProviderFactory.getOsgeoWmsTileProvider();
-        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(wmsTileProvider));
-
-        // Because the demo WMS layer we are using is just a white background map, switch the base layer
-        // to satellite so we can see the WMS overlay.
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap googleMap) {
+                        TileProvider wmsTileProvider = TileProviderFactory.getOsgeoWmsTileProvider();
+                        googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(wmsTileProvider));
+                    }
+                });
     }
 }
